cae696fdc08e4d4230e7886edc8cc29335c919b7
Ashmin Bhujel <ashminbhujel01@gmail.com>
2025-06-23 17:59:03 +0545

    feat: theme for Shadcn UI

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,17 @@
+import { ModeToggle } from "./components/mode-toggle";
+import { ThemeProvider } from "./components/theme-provider";
 import { Button } from "./components/ui/button";
 
 export default function App() {
   return (
-    <main className="flex flex-col">
-      <h1 className="p-8 text-center text-4xl font-bold tracking-tighter">
-        React Blog
-      </h1>
-      <Button className="mx-auto w-fit">Hello Shadcn UI</Button>
-    </main>
+    <ThemeProvider defaultTheme="system" storageKey="vite-ui-theme">
+      <main className="flex flex-col items-center gap-4">
+        <h1 className="p-8 text-center text-4xl font-bold tracking-tighter">
+          React Blog
+        </h1>
+        <ModeToggle />
+        <Button className="mx-auto w-fit">Hello Shadcn UI</Button>
+      </main>
+    </ThemeProvider>
   );
 }
