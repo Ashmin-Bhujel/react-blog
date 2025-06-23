e0ab2700003b984b716ee5b59376b71048603b08
Ashmin Bhujel <ashminbhujel01@gmail.com>
2025-06-23 17:46:26 +0545

    feat: integrate Shadcn UI

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,12 @@
+import { Button } from "./components/ui/button";
+
 export default function App() {
   return (
-    <main>
-      <h1>React Blog</h1>
+    <main className="flex flex-col">
+      <h1 className="p-8 text-center text-4xl font-bold tracking-tighter">
+        React Blog
+      </h1>
+      <Button className="mx-auto w-fit">Hello Shadcn UI</Button>
     </main>
   );
 }
